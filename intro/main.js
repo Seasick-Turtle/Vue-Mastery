b733c4a196bdff8a9ff7c9fbfe5e19042303cc48
Christopher Martin <martincdev@gmail.com>
2019-02-21 21:02:43 -0600

    clean up challenge 2

--- intro/main.js
+++ intro/main.js
@@ -2,7 +2,6 @@ let app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    image: './vmSocks-green-onWhite.jpg',
-    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+    image: './vmSocks-green-onWhite.jpg'
   }
 });
