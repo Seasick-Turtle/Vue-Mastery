ec069343ac7804c42b7bc52b404b398fc29974fa
Christopher Martin <martincdev@gmail.com>
2019-02-21 20:49:00 -0600

    finish lesson 2

--- intro/main.js
+++ intro/main.js
@@ -1,7 +1,7 @@
 let app = new Vue({
   el: '#app',
   data: {
-    product: 'Product goes here',
-    description: 'A pair of warm, fuzzy socks'
+    product: 'Socks',
+    image: './vmSocks-green-onWhite.jpg'
   }
 });
